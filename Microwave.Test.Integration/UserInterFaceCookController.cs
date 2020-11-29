0347b35954e70a4eb2f5731dcf5a89e0b0f6ec20
andreasstove <andreasstove@outlook.dk>
2020-11-30 00:21:34 +0100

    Test

---
+++ Microwave.Test.Integration/UserInterFaceCookController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class UserInterFaceCookController
+    {
+        //real
+        private Output _output;
+        private PowerTube _powerTube;
+        private Display _display;
+        private Light _light;
+        private UserInterface _userInterface;
+        private CookController _cookController;
+        private Timer _timer;
+
+        private System.IO.StringWriter _stringWriter;
+
+        //stubs
+
+        private IDoor _door;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _cancelButton;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _door = Substitute.For<IDoor>();
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _cancelButton = Substitute.For<IButton>();
+            _userInterface = new UserInterface(_powerButton, _timeButton, _cancelButton, _door, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+            _stringWriter = new System.IO.StringWriter();
+            Console.SetOut(_stringWriter);
+
+        }
+
+        [Test]
+        public void OnTimerExpiredWithUserInterFace()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+            _cancelButton.Pressed += Raise.Event();
+            //_timer.TimerTick += Raise.Event();
+            //_cookController.OnTimerExpired;
+            StringAssert.Contains("Light is turned off", _stringWriter.ToString());
+        }
+
+
+
+        [Test]
+        public void NotATestJustForHelp()
+        {
+            string expected = "Light is turned off";
+            _powerButton.Pressed += Raise.Event();
+            _light.TurnOn();
+            _stringWriter = new System.IO.StringWriter();
+            Console.SetOut(_stringWriter);
+            _cancelButton.Pressed += Raise.Event();
+
+            StringAssert.Contains(expected, _stringWriter.ToString());
+        }
+    }
+}
