7ef62e91ef70130453a2c3b8a8497d9a628348cb
andreasstove <andreasstove@outlook.dk>
2020-11-30 12:12:42 +0100

    test for knapper og door

---
+++ Microwave.Test.Integration/DoorUserInterFace.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+    class DoorUserInterFace
+    {
+        //real
+        private Output _output;
+        private PowerTube _powerTube;
+        private Display _display;
+        private Light _light;
+        private UserInterface _userInterface;
+        private CookController _cookController;
+        private Timer _timer;
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _cancelButton;
+        private Door _door;
+        private System.IO.StringWriter _stringWriter;
+
+        //stubs
+
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _cancelButton = new Button();
+            _door = new Door();
+            _cookController = new CookController(_timer, _display, _powerTube);
+
+
+            _userInterface = new UserInterface(_powerButton, _timeButton, _cancelButton, _door, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+            _stringWriter = new System.IO.StringWriter();
+            Console.SetOut(_stringWriter);
+
+        }
+
+        [Test]
+        public void DoorOpened()
+        {
+            _door.Open();
+            StringAssert.Contains("Light is turned on", _stringWriter.ToString());
+        }
+        [Test]
+        public void DoorOpenedClosed()
+        {
+            _door.Open();
+            _door.Close();
+            StringAssert.Contains("Light is turned on", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void DoorOpenedInSetPowerState()
+        {
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _door.Open();
+            StringAssert.Contains("Display cleared", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void DoorOpenedInSetTimeState()
+        {
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _door.Open();
+            StringAssert.Contains("Display cleared", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void DoorOpenedInCookingState()
+        {
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _cancelButton.Press();
+            _door.Open();
+            StringAssert.Contains("Display cleared", _stringWriter.ToString());
+        }
+        
+        
+        
+        [Test]
+        public void DoorOpenedAfterCookingIsDone()
+        {
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _cancelButton.Press();
+            System.Threading.Thread.Sleep(65000);
+            System.IO.StringWriter stringWriter1 = new System.IO.StringWriter();
+            Console.SetOut(stringWriter1);
+            _door.Open();
+            StringAssert.Contains("Light is turned off", _stringWriter.ToString());
+        }
+
+
+        [Test]
+        public void DoorClosedAfterCookingIsDone()
+        {
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _cancelButton.Press();
+            System.Threading.Thread.Sleep(65000);
+            System.IO.StringWriter stringWriter1 = new System.IO.StringWriter();
+            Console.SetOut(stringWriter1);
+            _door.Open();
+            _door.Close();
+            StringAssert.Contains("Light is turned off", _stringWriter.ToString());
+        }
+    }
+}
---
+++ Microwave.Test.Integration/PowerButtonUserInterFace.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class PowerButtonUserInterFace
+    {
+        //real
+        private Output _output;
+        private PowerTube _powerTube;
+        private Display _display;
+        private Light _light;
+        private UserInterface _userInterface;
+        private CookController _cookController;
+        private Timer _timer;
+        private Button _powerButton;
+        private System.IO.StringWriter _stringWriter;
+
+        //stubs
+
+        private IDoor _door;
+
+        private IButton _timeButton;
+        private IButton _cancelButton;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _powerButton = new Button();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _door = Substitute.For<IDoor>();
+
+            _timeButton = Substitute.For<IButton>();
+            _cancelButton = Substitute.For<IButton>();
+            _userInterface = new UserInterface(_powerButton, _timeButton, _cancelButton, _door, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+            _stringWriter = new System.IO.StringWriter();
+            Console.SetOut(_stringWriter);
+
+        }
+
+        [Test]
+        public void PowerButton1TimeUserInterFace()
+        {
+            _door.Opened += Raise.Event();
+            _door.Closed += Raise.Event();
+            _powerButton.Press();
+            StringAssert.Contains("50 W", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void PowerButton5TimeUserInterFace()
+        {
+            _door.Opened += Raise.Event();
+            _door.Closed += Raise.Event();
+            for (int i = 0; i < 5; i++)
+            {
+                _powerButton.Press();
+            }
+
+            StringAssert.Contains("250 W", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void PowerButton14TimeUserInterFace()
+        {
+            _door.Opened += Raise.Event();
+            _door.Closed += Raise.Event();
+            for (int i = 0; i < 14; i++)
+            {
+                _powerButton.Press();
+            }
+
+            StringAssert.Contains("700 W", _stringWriter.ToString());
+        }
+
+        [Test]
+        public void PowerButton15TimeUserInterFace()
+        {
+            _door.Opened += Raise.Event();
+            _door.Closed += Raise.Event();
+            for (int i = 0; i < 14; i++)
+            {
+                _powerButton.Press();
+            }
+
+            StringAssert.Contains("50 W", _stringWriter.ToString());
+        }
+
+
+
+
+
+
+    }
+}
